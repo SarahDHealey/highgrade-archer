3aa25f74debe85e96c106e30611d92b3f413e19d
sarah <s.deane.healey@gmail.com>
2014-03-22 13:19:51 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Highgrade1App::Application.config.secret_key_base = 'f5e16a90e834a84543bc3ed648ad4f17f93614bcebf7014b969c179728ac6a78e9a54e6d9d350df50ed06b324cab122ecf65f8fe38adf882175e6e1ae0da906a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Highgrade1App::Application.config.session_store :cookie_store, key: '_Highgrade1_app_session'
